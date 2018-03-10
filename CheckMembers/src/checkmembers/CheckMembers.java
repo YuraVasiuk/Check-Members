284a61be4d0ea67e5d1eb6b8ade82b7be742d861
YuriiVasiuk <vas14001@byui.edu>
2018-03-09 21:57:37 -0700

    First Commit

---
+++ CheckMembers/src/checkmembers/CheckMembers.java
@@ -0,0 +1,12 @@
+package checkmembers;
+
+public class CheckMembers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+    
+}
